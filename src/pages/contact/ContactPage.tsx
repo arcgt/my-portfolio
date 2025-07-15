02aae28a639e2d6b2f47d567b9cfc7470008b61f
arcgt <30797041+arcgt@users.noreply.github.com>
2025-07-15 20:45:49 +0100

    add assets, update colour scheme

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactPage: React.FC = () => {
   return (
-    <section className='bg-white p-4 text-black md:p-8'>
+    <section className='bg-slate-50 p-4 text-slate-950 md:p-8'>
       <div className='mx-auto flex flex-col gap-4'>
         <h1>contact</h1>
       </div>
--- src/pages/home/components/ExampleComponent.tsx
+++ src/pages/home/components/ExampleComponent.tsx
@@ -8,7 +8,7 @@ const ExampleComponent: React.FC<ExampleComponentProps> = (props) => {
   const { exampleString } = props;
 
   return (
-    <section className='min-h-dvh-nav bg-white p-4 text-black md:p-8'>
+    <section className='min-h-dvh-nav bg-slate-50 p-4 text-slate-950 md:p-8'>
       <div className='mx-auto flex flex-col gap-4'>
         <h1>{exampleString}</h1>
       </div>
