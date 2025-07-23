1f172890a7fc4c46fdcb3224f6c24567bb9f6ec0
arcgt <tianhonggan@outlook.com>
2025-07-23 23:13:41 +0100

    fix unknown page

--- src/pages/unknown/UnknownPage.tsx
+++ src/pages/unknown/UnknownPage.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const KnownPage: React.FC = () => {
   return (
-    <section className='bg-white p-4 text-black xl:p-8'>
+    <section className='flex flex-row w-screen items-center justify-center'>
       <div className='flex flex-col gap-4'>
-        <h1>unknown</h1>
+        <h1>404</h1>
+        <p className='text-4xl'>Oops... Page not found.</p>
       </div>
     </section>
   );
